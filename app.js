35e4ac399887af54c29c0924d7f41e55fa0f9d66
Wolfleader <james.morgan@iinet.net.au>
2020-03-05 18:09:54 +0800

    made it a promise

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
-const fs = require('fs')
 
 axios.defaults.baseURL = 'https://api.battlemetrics.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 module.exports = {
     GetServerInfoByName: (serverName, gameType, serverStatus = "online") => {
-        axios.get(`/servers?filter[search]='${serverName}&filter[game]=${gameType}&filter[status]=${serverStatus}`)
+        return axios.get(`/servers?filter[search]='${serverName}&filter[game]=${gameType}&filter[status]=${serverStatus}`)
             .then(res => {
                 let servers = [];
                 res.data.data.forEach(el => {
@@ -20,10 +19,7 @@ module.exports = {
                     }
                     servers.push(info);
                 });
-                fs.writeFile('server.json', JSON.stringify(servers), (err) => {
-                    if (err) throw err;
-                    console.log("Retrieved Server info!")
-                })
+                return servers;
             })
     },
 
